4abbaa858f826db4d9ab86798a473d02a160cd39
vietcoder1998 <duyviet2841998@gmail.com>
2025-10-01 22:26:31 +0700

    rebase migrate db

--- prisma/migrations/20250918153437_add_assetid_to_permission/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `permission` ADD COLUMN `assetId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `permission` ADD CONSTRAINT `permission_assetId_fkey` FOREIGN KEY (`assetId`) REFERENCES `asset`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20250925171232_add_position/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `gantttask` ADD COLUMN `position` INTEGER NULL;
